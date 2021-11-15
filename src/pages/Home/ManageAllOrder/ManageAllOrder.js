6e8e059c00d68e14b2f723c771be3e9178587be6
leader with sadiq <gm1sadiq@gmail.com>
2021-11-15 17:10:49 +0600

    fun commit

--- src/pages/Home/ManageAllOrder/ManageAllOrder.js
+++ src/pages/Home/ManageAllOrder/ManageAllOrder.js
@@ -32,7 +32,7 @@ const handleDelete = (id) => {
             <h2>Mange All Orders: {orders?.length}</h2>
             <div className='container'>
             <div className="row">
-                <div className="col-md-8 mx-auto">
+                <div className="col-md-8 col-sm-6 mx-auto">
                 <Table striped bordered hover>
   <thead>
     <tr>
